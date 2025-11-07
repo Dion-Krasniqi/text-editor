8f49ddd093f86f8da17a9d427d4eeb3b259f7a36
Dion-Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2025-11-07 08:36:05 +0100

    Disabled echo on terminal when program is running, goes back to how it was on exit

--- editor.c
+++ editor.c
@@ -1,6 +1,26 @@
+#include <stdlib.h>
 #include <unistd.h>
+#include <termios.h>
+
+struct termios og_termios;
+
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &og_termios);
+}
+
+void enableRawMode() {
+  tcgetattr(STDIN_FILENO, &og_termios);
+  atexit(disableRawMode);
+
+  struct termios raw = og_termios;
+
+  raw.c_lflag &= ~(ECHO);
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main() {
+  enableRawMode();
   char c;
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
   return 0;
