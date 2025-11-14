c4c3f4ad0bff53ad618e939e03e7982efc032bcb
Dion-Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2025-11-14 23:08:33 +0100

    writing the tiddles from the buffer

--- editor.c
+++ editor.c
@@ -1,6 +1,7 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <errno.h>
 #include <termios.h>
@@ -100,24 +101,51 @@ void editorProcessKeypress() {
       break;
   }
 }
+/*** append buffer ***/
+
+struct abuf {
+  char *b;
+  int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+  char *new = realloc(ab->b, ab->len + len);
+  if (new == NULL) return;
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab->len += len;
+}
+
+void abFree(struct abuf *ab){
+  free(ab->b);
+}
+
+
 /**output**/
-void editorDrawRows() {
+void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y<E.screenrows; y++) {
-    write(STDOUT_FILENO, "~\r\n", 1);
+    abAppend(ab, "~", 1);
+    //write(STDOUT_FILENO, "~\r\n", 1);
     if (y < E.screenrows - 1) {
-      write(STDOUT_FILENO, "\r\n", 2);
+      abAppend(ab, "\r\n", 2);
+     //write(STDOUT_FILENO, "\r\n", 2);
     }
 
   }
 }
 
 void editorRefreshScreen() {
-  write(STDOUT_FILENO, "\x1b[2j", 4);
-  write(STDOUT_FILENO, "\x1b[H", 3);
-  editorDrawRows();
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  struct abuf ab = ABUF_INIT;
+  abAppend(&ab, "\x1b[2J", 4);
+  abAppend(&ab, "\x1b[H", 3);
 
+  editorDrawRows(&ab);
+  abAppend(&ab, "\x1b[H",3);
+  write(STDOUT_FILENO, ab.b, ab.len);
+  abFree(&ab);
 }
 
 
